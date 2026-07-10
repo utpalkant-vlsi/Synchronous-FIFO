feaeae043a8afa1ef34cbe279b4d40cf1bd3faff
Utpal Kant <utpalkant1227@gmail.com>
2026-07-11 00:17:22 +0530

    Add files via upload

---
+++ sim_1/new/Synchronous_FIFO_tb.v
@@ -0,0 +1,106 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.04.2026 14:49:10
+// Design Name: 
+// Module Name: SYNC_FIFO_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SYNC_FIFO_tb();
+parameter FIFO_DEPTH = 8;
+parameter DATA_WIDTH = 32;
+reg clk,rst_n,read_enable,write_enable;
+reg[31:0] write_data;
+wire[31:0] read_data;
+wire FULL,EMPTY;
+
+integer i;
+
+SYNC_FIFO #(.DATA_WIDTH (32), .FIFO_DEPTH (8), .POINTER_WIDTH (3))
+    FIFO( 
+    .clk(clk),
+    .rst_n(rst_n),
+    .write_data(write_data),
+    .read_data(read_data),
+    .read_enable(read_enable),
+    .write_enable(write_enable),
+    .FULL(FULL),
+    .EMPTY(EMPTY)
+    );
+    
+ initial clk = 0;
+ always #5 clk = ~clk;
+ 
+ // TASK 1 WRITE ONE WORD INTO FIFO;
+ 
+ task fifo_write;
+      input[31:0] data;
+      begin
+           @(posedge clk);
+           write_enable = 1;
+           write_data = data;
+           $display($time,"  write_data data_in = %0d", write_data);
+            @(posedge clk)
+            write_enable = 0;
+      end
+ endtask
+ 
+ // TAASK 2 READ ONE WORD AND SELF-CHECK AGAINST EXPECTED
+ 
+   task fifo_read();
+        begin 
+             @(posedge clk);
+             read_enable = 1;
+             @(posedge clk);
+             #1;
+             $display($time,"  read_data data_out = %0d",read_data);
+             read_enable = 0;
+             end
+   endtask
+        
+   initial begin 
+   #1;
+   rst_n = 0; read_enable = 0; write_enable = 0;
+   
+   #1.3;
+   rst_n = 1;
+   $display($time, " \n SCENARIO 1");
+  fifo_write(1);
+   fifo_write(10);
+  fifo_write(100);
+  fifo_read();
+  fifo_read();
+  fifo_read();
+  fifo_read();
+   
+   
+   $display($time, " \n SCENARIO 2");
+   for(i = 0; i<FIFO_DEPTH; i = i+1)begin
+      fifo_write(2**i);
+      fifo_read();
+    end
+    
+    $display($time," \n SCENARIO 3");
+    for(i = 0; i<FIFO_DEPTH; i = i+1)begin
+       fifo_write(2**i);
+    end
+    for(i = 0; i<FIFO_DEPTH; i = i+1)begin
+       fifo_read();
+    end
+#40 $stop;
+end
+endmodule
---
+++ sources_1/new/SYNC_FIFO.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.04.2026 12:24:33
+// Design Name: 
+// Module Name: SYNC_FIFO
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SYNC_FIFO #(
+parameter DATA_WIDTH = 32,// PARAMETER DECLARATION
+parameter FIFO_DEPTH = 8) // PARAMETER DECLARATION
+//parameter POINTER_WIDTH = 3)  // PARAMETER DECLARATION
+
+(input clk,rst_n,write_enable,read_enable,                // INPUT-OUTPUT DECLARATION
+input [DATA_WIDTH-1:0] write_data,              // DATA TO WRITE
+output reg [DATA_WIDTH-1:0] read_data,         // DATA TO READ
+output FULL,EMPTY);
+
+localparam FIFO_DEPTH_LOG = $clog2(FIFO_DEPTH);
+reg[DATA_WIDTH-1:0]mem[0:FIFO_DEPTH-1];  //8 LOCATION EACH 32 BITS WIDE
+
+reg [FIFO_DEPTH_LOG:0] write_pointer;   //POINTS TO NEXT WRITE LOCATION
+reg [FIFO_DEPTH_LOG:0] read_pointer;   //POINTS TO NEXT READ LOACTION
+//reg [POINTER_WIDTH-1:0] count;    //PTR_WIDTH+1 BITS TO COUNT 0 TO 8
+
+assign  FULL = (read_pointer=={~write_pointer[FIFO_DEPTH_LOG],write_pointer[FIFO_DEPTH_LOG-1:0]} );  
+assign EMPTY = (read_pointer == write_pointer);
+
+always@(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+              write_pointer <= 0;
+        else if (write_enable && !FULL) 
+              write_pointer <= write_pointer + 1'b1;
+end
+
+always@(posedge clk or negedge rst_n)begin
+       if(!rst_n)
+             read_pointer <= 0;
+       else if (read_enable && !EMPTY)
+              read_pointer<= read_pointer +1;
+end
+
+always@(posedge clk)begin
+        if(write_enable && ~FULL )
+           mem[write_pointer[FIFO_DEPTH_LOG-1:0]] <= write_data;
+ end
+ 
+ always@(posedge clk or negedge rst_n) begin
+        if(!rst_n)
+           read_data <= 0;
+        else if (read_enable && !EMPTY)
+           read_data <= mem[read_pointer [FIFO_DEPTH_LOG-1:0]];
+ end
+endmodule
